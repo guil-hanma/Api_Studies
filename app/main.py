6b1f2e1e70110734743718d59896001d6be153ab
Guil-Hanma <guilherme.lima170@hotmail.com>
2023-10-11 15:04:13 -0300

    Main.py changed path and folder app with init.py created for future use

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def get_posts():
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
-    post_dict["id"] = randrange(0, 10)
+    post_dict["id"] = randrange(0, 10000000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
 
@@ -68,3 +68,16 @@ def delete_post(id: int):
                             detail=f"post with id {id} was not found")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
